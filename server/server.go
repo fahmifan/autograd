4977d74e2168686dd4cda29aa86371de5ac5c2bd
IbnuAhsani <ibnu.muhari@gmail.com>
2020-10-22 22:16:27 +0700

    feat: init web server

---
+++ server/server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Helo, world!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
