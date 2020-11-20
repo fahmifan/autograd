62cd1c90e0936a78f3da92e13539d00e0bffd89b
IbnuAhsani <ibnu.muhari@gmail.com>
2020-11-20 11:31:59 +0700

    style: add quotes to table fields in all migrations

--- db/migrations/000002_create_assignments_table.up.sql
+++ db/migrations/000002_create_assignments_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS "assignments" (
-    id BIGINT PRIMARY KEY,
+    "id" BIGINT PRIMARY KEY,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    case_output_file TEXT NOT NULL,
-    case_input_file TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL,
-    deleted_at TIMESTAMPTZ
+    "case_output_file" TEXT NOT NULL,
+    "case_input_file" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+    "deleted_at" TIMESTAMPTZ
 );
