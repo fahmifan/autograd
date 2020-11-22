e6f9079e9d82b7d5184c262aa577c36f6a6cd75a
IbnuAhsani <ibnu.muhari@gmail.com>
2020-11-22 12:50:12 +0700

    feat: add created_by field in assignments migration

--- db/migrations/000002_create_assignments_table.up.sql
+++ db/migrations/000002_create_assignments_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS "assignments" (
     "id" BIGINT PRIMARY KEY,
+    "created_by" BIGINT REFERENCES users(id) NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "case_output_file" TEXT NOT NULL,
