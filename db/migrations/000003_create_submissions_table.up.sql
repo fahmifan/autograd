e48ce064884d0739f309b86c8ef254ae52b38129
IbnuAhsani <ibnu.muhari@gmail.com>
2020-11-22 22:17:20 +0700

    feat: add is_graded field in submissions migration

--- db/migrations/000003_create_submissions_table.up.sql
+++ db/migrations/000003_create_submissions_table.up.sql
@@ -2,9 +2,10 @@ CREATE TABLE IF NOT EXISTS "submissions" (
     "id" BIGINT PRIMARY KEY,
     "assignment_id" BIGINT REFERENCES assignments(id) NOT NULL,
     "submitted_by" BIGINT REFERENCES users(id) NOT NULL,
-    "file_url" TEXT NOT NULL,
+    "is_graded" BOOLEAN DEFAULT FALSE NOT NULL,
     "grade" NUMERIC(3, 2) DEFAULT 0 NOT NULL,
-    "feedback" TEXT DEFAULT '',
+    "feedback" TEXT DEFAULT '' NOT NULL,
+    "file_url" TEXT DEFAULT '' NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL,
     "updated_at" TIMESTAMPTZ NOT NULL,
     "deleted_at" TIMESTAMPTZ
