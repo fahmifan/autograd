ee2511a77e87c108e7545c363dc664082510d62c
IbnuAhsani <ibnu.muhari@gmail.com>
2020-12-05 11:43:26 +0700

    refactor: move req, res structs & funcs to a seperate file

--- httpsvc/submission_handler.go
+++ httpsvc/submission_handler.go
@@ -2,7 +2,6 @@ package httpsvc
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/miun173/autograd/model"
 	"github.com/miun173/autograd/utils"
@@ -10,46 +9,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
 )
-
-// SubmissionRequest ..
-type submissionRequest struct {
-	AssignmentID int64  `json:"assignmentID"`
-	SubmittedBy  int64  `json:"submittedBy"`
-	FileURL      string `json:"fileURL"`
-}
-
-// SubmissionResponse ..
-type submissionResponse struct {
-	ID           string  `json:"id"`
-	AssignmentID string  `json:"assignmentID"`
-	SubmittedBy  string  `json:"submittedBy"`
-	FileURL      string  `json:"fileURL"`
-	Grade        float64 `json:"grade"`
-	Feedback     string  `json:"feedback"`
-	CreatedAt    string  `json:"createdAt"`
-	UpdatedAt    string  `json:"updatedAt"`
-}
-
-func submissionRequestToModel(r *submissionRequest) *model.Submission {
-	return &model.Submission{
-		AssignmentID: r.AssignmentID,
-		SubmittedBy:  r.SubmittedBy,
-		FileURL:      r.FileURL,
-	}
-}
-
-func submissionModelToResponse(m *model.Submission) *submissionResponse {
-	return &submissionResponse{
-		ID:           utils.Int64ToString(m.ID),
-		AssignmentID: utils.Int64ToString(m.AssignmentID),
-		SubmittedBy:  utils.Int64ToString(m.SubmittedBy),
-		FileURL:      m.FileURL,
-		Grade:        m.Grade,
-		Feedback:     m.Feedback,
-		CreatedAt:    m.CreatedAt.Format(time.RFC3339Nano),
-		UpdatedAt:    m.UpdatedAt.Format(time.RFC3339Nano),
-	}
-}
 
 func (s *Server) handleCreateSubmission(c echo.Context) error {
 	submissionReq := &submissionRequest{}
@@ -69,18 +28,6 @@ func (s *Server) handleCreateSubmission(c echo.Context) error {
 	return c.JSON(http.StatusOK, submissionModelToResponse(submission))
 }
 
-func uploadRequestToModel(r *model.UploadRequest) *model.Upload {
-	return &model.Upload{
-		SourceCode: r.SourceCode,
-	}
-}
-
-func uploadModelToResponse(m *model.Upload) *model.UploadResponse {
-	return &model.UploadResponse{
-		FileURL: m.FileURL,
-	}
-}
-
 func (s *Server) handleUpload(c echo.Context) error {
 	uploadReq := &model.UploadRequest{}
 	err := c.Bind(uploadReq)
@@ -97,34 +44,6 @@ func (s *Server) handleUpload(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, uploadModelToResponse(upload))
-}
-
-type cursorResponse struct {
-	Size      int64       `json:"size"`
-	Page      int64       `json:"page"`
-	Sort      string      `json:"sort"`
-	TotalPage int64       `json:"totalPage"`
-	TotalData int64       `json:"totalData"`
-	Data      interface{} `json:"data"`
-}
-
-func newSubmissionResponses(submissions []*model.Submission) (submissionRes []*submissionResponse) {
-	for _, submission := range submissions {
-		submissionRes = append(submissionRes, submissionModelToResponse(submission))
-	}
-
-	return
-}
-
-func newCursorResponse(c model.Cursor, data interface{}, count int64) *cursorResponse {
-	return &cursorResponse{
-		Size:      c.GetSize(),
-		Page:      c.GetPage(),
-		Sort:      c.GetSort(),
-		TotalPage: c.GetTotalPage(count),
-		TotalData: count,
-		Data:      data,
-	}
 }
 
 func (s *Server) handleGetAssignmentSubmission(c echo.Context) error {
