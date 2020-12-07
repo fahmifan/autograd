7d599e7b51b0de91246513808b9fbe48fbe4ca28
IbnuAhsani <ibnu.muhari@gmail.com>
2020-12-07 22:49:51 +0700

    fix: rename wrongly named assignments table fields

--- db/migrations/000002_create_assignments_table.up.sql
+++ db/migrations/000002_create_assignments_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS "assignments" (
     "id" BIGINT PRIMARY KEY,
-    "created_by" BIGINT REFERENCES users(id) NOT NULL,
+    "assigned_by" BIGINT REFERENCES users(id) NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "case_output_file" TEXT NOT NULL,
-    "case_input_file" TEXT NOT NULL,
+    "case_output_file_url" TEXT NOT NULL,
+    "case_input_file_url" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL,
     "updated_at" TIMESTAMPTZ NOT NULL,
     "deleted_at" TIMESTAMPTZ
