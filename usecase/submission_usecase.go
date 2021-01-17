4d4e286a5a5400b1ed1ee20b478f93bbead1fb70
fahmifan <fahmifan173@gmail.com>
2021-01-17 21:09:39 +0700

    refactor: move upload to media

--- usecase/submission_usecase.go
+++ usecase/submission_usecase.go
@@ -6,11 +6,8 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math/rand"
-	"os"
-	"path"
 	"time"
 
-	"github.com/miun173/autograd/config"
 	"github.com/miun173/autograd/model"
 	"github.com/miun173/autograd/repository"
 	"github.com/miun173/autograd/utils"
@@ -24,7 +21,6 @@ type SubmissionUsecase interface {
 	DeleteByID(ctx context.Context, id int64) (*model.Submission, error)
 	FindByID(ctx context.Context, id int64) (*model.Submission, error)
 	Update(ctx context.Context, submission *model.Submission) error
-	Upload(ctx context.Context, sourceCode string) (string, error)
 	FindAllByAssignmentID(ctx context.Context, cursor model.Cursor, assignmentID int64) (submissions []*model.Submission, count int64, err error)
 	UpdateGradeByID(ctx context.Context, id, grade int64) error
 }
@@ -151,38 +147,6 @@ func (s *submissionUsecase) Update(ctx context.Context, submission *model.Submis
 	}(submission.ID)
 
 	return nil
-}
-
-func (s *submissionUsecase) Upload(ctx context.Context, sourceCode string) (string, error) {
-	if sourceCode == "" {
-		return "", ErrInvalidArguments
-	}
-
-	logger := logrus.WithFields(logrus.Fields{
-		"ctx":        utils.Dump(ctx),
-		"sourceCode": sourceCode,
-	})
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		logger.Error(err)
-		return "", err
-	}
-
-	fileName := generateFileName() + ".cpp"
-	filePath := path.Join(cwd, "submission", fileName)
-	file, err := os.Create(filePath)
-	if err != nil {
-		logger.Error(err)
-		return "", err
-	}
-
-	file.WriteString(sourceCode)
-	defer file.Close()
-
-	fileURL := config.BaseURL() + "/storage/" + fileName
-
-	return fileURL, nil
 }
 
 func generateFileName() string {
